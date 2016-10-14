e4c49677c07065111ea455b9fa2cb0c306adf32c
fy <fy0@qq.com>
2016-10-15 00:11:43 +0800

    根据使用情况轻微调整

--- backend/config.py
+++ backend/config.py
@@ -9,7 +9,7 @@ COOKIE_SECRET = "6aOO5ZC55LiN5pWj6ZW/5oGo77yM6Iqx5p+T5LiN6YCP5Lmh5oSB44CC"
 ORIGIN_DOMAIN = 'http://localhost:8080'
 
 TITLE_LENGTH_MIN = 1  # 发表主题的标题最短长度
-TITLE_LENGTH_MAX = 50  # 发表主题的标题最长长度
+TITLE_LENGTH_MAX = 100  # 发表主题的标题最长长度
 
 TOPIC_PAGE_SIZE = 20  # 每页主题数量
 REPLY_PAGE_SIZE = 30  # 每页评论数量
